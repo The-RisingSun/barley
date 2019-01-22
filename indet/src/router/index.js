d0a8b47c8d321c032f3fe0cfaad4da260735c7ce
fyan <43058534+The-RisingSun@users.noreply.github.com>
2019-01-22 18:29:04 +0800

    更改默认首页--王毅 --1/22/18：28

--- indet/src/router/index.js
+++ indet/src/router/index.js
@@ -1,18 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/xxy/Home'
-import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/home',
       name: 'Home',
       component: Home
     }
