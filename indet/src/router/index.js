c3fb5d89353b2736c1d396b9c88f270c205204e1
linjiadong <784519336@qq.com>
2019-01-23 00:10:52 +0800

    修改了List.vue 添加少量图片 修改了index的路由

--- indet/src/router/index.js
+++ indet/src/router/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/xxy/Home'
+import HelloWorld from '@/components/HelloWorld'
+import List from '@/components/ljd/List'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
