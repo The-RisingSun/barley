fdf4af5a2bb12119421a1634ff52ebf1c7c1494f
linjiadong <784519336@qq.com>
2019-01-22 21:11:31 +0800

    修改index冲突

--- indet/src/router/index.js
+++ indet/src/router/index.js
@@ -1,30 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< HEAD
-
-import HelloWorld from '@/components/HelloWorld'
-import List from '@/components/ljd/List'
-=======
 import Home from '@/components/xxy/Home'
->>>>>>> d0a8b47c8d321c032f3fe0cfaad4da260735c7ce
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-<<<<<<< HEAD
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/list',
-      name: 'List',
-      component: List
-=======
       name: 'Home',
       component: Home
->>>>>>> d0a8b47c8d321c032f3fe0cfaad4da260735c7ce
     }
   ]
 })
