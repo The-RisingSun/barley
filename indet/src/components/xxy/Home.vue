63712b63d06dc38a2a3d907257af78557cdb5e8e
xuxiaoyang883 <245020153@qq.com>
2019-01-30 21:58:04 +0800

    提交

---
+++ indet/src/components/xxy/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="home">
+      <img src="http://project.kgc.cn/#/static/img/pic1.jpg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
--- indet/src/router/index.js
+++ indet/src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
 import HelloWorld from '@/components/HelloWorld'
-
+import Home from "../components/xxy/Home";
+import Children from "../components/xxy/Children"
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +11,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path:'/home',
+      name:'Home',
+      component:Home
+    },
+    {
+      path:'/children',
+      name:'Children',
+      component:Children
     }
   ]
 })
